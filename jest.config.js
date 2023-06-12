bad0f6e46ed32c8cacde847a58e76403cf573143
srinu <srinu11112000@gmail.com>
2023-06-12 10:29:27 +0530

    run test

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,7 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  testMatch: ["**/**/*.test.ts"],
+  testMatch: ["**/*.test.ts"],
   verbose: true,
   forceExit: true,
   // clearMocks: true,
